22077862ab35ac14560a2a374054e2e9cecf861a
Нина <vasilenko.lesnik2017@yandex.ru>
2021-03-06 22:01:28 +0900

    5. Конвертер валюты [#486575]

---
+++ src/ru/job4j/condition/Point.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+public class Point {
+    public static double distance(int x1, int y1, int x2, int y2) {
+        double rls = x2 - x1;
+        double first1 = Math.pow(rls, 2);
+        double rls2 = y2 - y1;
+        double first2 = Math.pow(rls2, 2);
+        double lenght = first1 + first2;
+        double root = Math.sqrt(lenght);
+        return root;
+    }
+
+    public static void main(String[] agrg) {
+        double result = Point.distance(1,3,3,4);
+        System.out.println("result (0, 0) to (2, 0) " + result);
+    }
+
+}
