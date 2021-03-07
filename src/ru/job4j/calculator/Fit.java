7098e3f24f585a956ec0d351b5c67ebadbd594d9
Нина <vasilenko.lesnik2017@yandex.ru>
2021-03-07 15:14:45 +0900

    6. Идеальный вес [#486576]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -4,11 +4,13 @@ public class Fit {
 
     public static double manWeight(short height) {
                 return (height - 100) * 1.15;
-            }
-            public static double womanWeight(short height) {
+}
+
+public static double womanWeight(short height) {
 return (height - 110) * 1.15;
-            }
-           public static void main (String[] args) {
+    }
+
+public static void main(String[] args) {
             short height = 178;
             double man = Fit.manWeight(height);
             double woman = Fit.womanWeight(height);
--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -2,16 +2,15 @@ package ru.job4j.converter;
 
 public class Converter {
 
-    public static int rubleToEuro(int value) {
+public static int rubleToEuro(int value) {
                 return value / 70;
         }
 
-    public static int rubleToDollar(int value) {
+public static int rubleToDollar(int value) {
                 return value / 60;
         }
 
-    public static void main(String[] args) {
-
+public static void main(String[] args) {
         int euro = Converter.rubleToEuro(280);
         int dollar = Converter.rubleToDollar(120);
         System.out.println("280 rubles " + euro + " euro");
