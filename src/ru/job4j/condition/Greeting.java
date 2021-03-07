1e0b3fc71d4aa24c77985fbf89071970d13f613e
Нина <vasilenko.lesnik2017@yandex.ru>
2021-03-07 11:24:33 +0900

    9.1. String [#486586]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,10 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like java";
+        int year = 2021;
+        System.out.println(idea + " But i am a newbie.");
+        System.out.println(idea + " " + year);
+    }
+}
