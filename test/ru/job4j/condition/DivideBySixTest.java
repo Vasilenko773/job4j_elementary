38e28d38db7df104784e0210c84c420bf9479a4e
Нина <vasilenko.lesnik2017@yandex.ru>
2021-03-13 12:20:35 +0900

    4.4. Вложенный оператор if else [#486537]

--- test/ru/job4j/condition/DivideBySixTest.java
+++ test/ru/job4j/condition/DivideBySixTest.java
@@ -1,34 +1,38 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
-
 public class DivideBySixTest {
+    @Test
+    public void whenNumberDivideBy6() {
+        int in = 24;
+        String result = DivideBySix.checkNumber(in);
+        String expected = "Исходное число делится на 6";
+        Assert.assertEquals(expected, result);
+    }
 
     @Test
-    public void whenNumberDivdeBy6() {
-        String expected = "Исходное число делится на 6";
-        assertThat(DivideBySix.checkNumber(24), is(expected));
-        }
-
-        @Test
     public void whenNumberDivideBy3AndNotEven() {
-        String expexted = "Исходное число делится на 3, но не является четным";
-        assertThat(DivideBySix.checkNumber(9), is(expexted));
-        }
+        int in = 9;
+        String result = DivideBySix.checkNumber(in);
+        String expected = "Исходное число делится на 3, но не является четным";
+        Assert.assertEquals(expected, result);
+    }
 
-        @Test
-    public void whenNumberNoDivideBy3AndEvrn() {
-        String expexted = "Исходное число не делится на 3 но является четным";
-        assertThat(DivideBySix.checkNumber(14), is(expexted));
-        }
+    @Test
+    public void whenNumberNoDivideBy3AndEven() {
+        int in = 14;
+        String result = DivideBySix.checkNumber(in);
+        String expected = "Исходное число не делится на 3 но является четным";
+        Assert.assertEquals(expected, result);
+    }
 
-        @Test
+    @Test
     public void whenNumberNoDivideBy3AndNotEven() {
-        String expexted = "Исходное число не делится на 3 и не является четным";
-        assertThat(DivideBySix.checkNumber(25), is(expexted));
-        }
+        int in = 25;
+        String result = DivideBySix.checkNumber(in);
+        String expected = "Исходное число не делится на 3 и не является четным";
+        Assert.assertEquals(expected, result);
+    }
 }
