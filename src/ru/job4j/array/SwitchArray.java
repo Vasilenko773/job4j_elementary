f7b99e03e60de2a378e5a8ee8d77e8b9108e5ba1
Нина <vasilenko.lesnik2017@yandex.ru>
2021-03-19 21:39:01 +0900

    6.2.2. Переставить элементы массива [#486562]

--- src/ru/job4j/array/SwitchArray.java
+++ src/ru/job4j/array/SwitchArray.java
@@ -3,8 +3,8 @@ package ru.job4j.array;
 public class SwitchArray {
     public static int[] swap(int[] array, int source, int dest) {
         int temp = array[source];
-        array[source] = array[dest - 1];
-        array[dest - 1] = temp;
+        array[source] = array[dest];
+        array[dest] = temp;
         return array;
     }
 
--- test/ru/job4j/array/SwitchArrayTest.java
+++ test/ru/job4j/array/SwitchArrayTest.java
@@ -17,11 +17,11 @@ public class SwitchArrayTest {
 
     @Test
     public void whemSwap1to4() {
-        int[] input = {1, 2, 3, 4};
+        int[] input = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int source = 1;
-        int dest = 4;
+        int dest = 8;
         int[] result = SwitchArray.swap(input, source, dest);
-        int[] expexted = {1, 4, 3, 2};
+        int[] expexted = {1, 9, 3, 4, 5, 6, 7, 8, 2, 10};
         Assert.assertArrayEquals(expexted, result);
     }
 
@@ -29,7 +29,7 @@ public class SwitchArrayTest {
     public void whemSwap3to8() {
         int[] input = {1, 2, 3, 4, 5, 6, 7, 8};
         int source = 2;
-        int dest = 8;
+        int dest = 7;
         int[] result = SwitchArray.swap(input, source, dest);
         int[] expexted = {1, 2, 8, 4, 5, 6, 7, 3};
         Assert.assertArrayEquals(expexted, result);
