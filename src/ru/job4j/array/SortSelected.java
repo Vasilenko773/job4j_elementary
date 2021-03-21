b2cced9ad813850f6a15385937288f1d008ad5f2
Нина <vasilenko.lesnik2017@yandex.ru>
2021-03-21 21:05:17 +0900

    6.5.3. Сортировка выборкой. [#486551]

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -5,15 +5,12 @@ public class SortSelected {
         for (int i = 0; i < data.length; i++) {
             int min = MinDiapason.findMin(data, i, data.length - 1);
             int j = FindLoop.indexOf(data, min, i, data.length - 1);
-            min = data[j]; // до сих пор правильно
-            int first = data[i];
+            min = data[j];
+          SwitchArray.swap(data, i, j);
+          /*  int first = data[i];
             data[i] = data[j];
-            data[j] = first; // после можно написать если (i[0] равно мини), минимум  = мин + 1
-            /*if (data[i] - data[j] == 1) {
-                int two = data[1];
-                data[1] = data[j];
-                data[j] = two;*/
-
+            data[j] = first; // это работает
+           */
         }
         return data;
     }
