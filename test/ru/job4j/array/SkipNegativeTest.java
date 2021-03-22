4a33efc76a3d26acbb42ed36b66d8a6b3bd64418
Нина <vasilenko.lesnik2017@yandex.ru>
2021-03-22 09:58:48 +0900

    6.6.3. Двухмерный массив. If. [#486566]

--- test/ru/job4j/array/SkipNegativeTest.java
+++ test/ru/job4j/array/SkipNegativeTest.java
@@ -36,4 +36,22 @@ public class SkipNegativeTest {
         };
         Assert.assertArrayEquals(expected, result);
     }
+
+    @Test
+    public void whenArrayRow5AndColumnNotEquals() {
+        int[][] array = {
+                {1, -2, 2},
+                {1, -2, -3, 2},
+                {1, -2, 3, -4},
+                {1, 2, -3, -4, 5, -6}
+        };
+        int[][] result = SkipNegative.skip(array);
+        int[][] expected = {
+                {1, 0, 2},
+                {1, 0, 0, 2},
+                {1, 0, 3, 0},
+                {1, 2, 0, 0, 5, 0}
+        };
+        Assert.assertArrayEquals(expected, result);
+    }
 }
